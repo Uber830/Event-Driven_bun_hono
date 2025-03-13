fe4befdc82b7a0b888834f78feb3bb8e2cbb2103
Uber830 <hhcardoso@misena.edu.co>
2025-03-12 23:13:32 -0500

    fix:🐛 Validar conexion
    Saber si la conexion se hace corectamente

--- Backend_2/src/config/db.ts
+++ Backend_2/src/config/db.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 
 // Creation of url connection
 const DATABASE_URL = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
+console.log(DATABASE_URL, "DATABASE_URL 2");
 const pool = new Pool({
   connectionString: DATABASE_URL,
   ssl: {
