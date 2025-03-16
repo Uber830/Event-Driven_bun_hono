c522b80f0424b2411e76f0f2ef3716a3f1449bbe
Uber830 <hhcardoso@misena.edu.co>
2025-03-16 15:58:07 -0500

    chore:🔥 Pasar correctamente parametros
    El servidor no estava tomando la variable del puerto correctamente

--- Backend_2/app.ts
+++ Backend_2/app.ts
@@ -1,6 +1,6 @@
 import home from "./src/index";
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT2 || 3000;
+console.log("PORT2", port);
 // run the app
 export default {
   port,
